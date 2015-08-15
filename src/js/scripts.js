bdc81f38be68f612bdd5dbd97bd863771ad512f5
Matt Steele <matt@omnionline.us>
2015-08-15 14:28:15 -0400

    sweepstakes link

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,32 @@
-//alert('test2');
+$(document).ready(function () {
+    'use strict';
+    
+    $('.modal-link').click(function (e) {
+        e.preventDefault();
+        
+        var $this = $(this),
+            content = $($this.attr('data-target')).html(),
+            $modal = $("<div class='modal'></di>"),
+            $modalcontent = $("<div class='modal-body'></div>"),
+            $body = $('body');
+
+        $modalcontent.append(content);
+        $modal.append($modalcontent);
+        
+        $modalcontent.on('click', function(e) {
+            e.stopPropagation();
+        });
+        
+        $modal.on('click', function () {
+            $modal.off('click');
+            $modalcontent.off('click');
+            $(this).remove();
+            $body.removeClass('noscroll');
+        });
+        
+        $body.prepend($modal);
+        $body.addClass('noscroll');
+        
+    });
+    
+});
